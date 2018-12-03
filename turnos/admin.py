ae96905225589cfc4b42d4144f8e9639ab96f4e0
atilio <atilio@dissoi.com>
2018-12-03 13:08:36 -0300

    NUTFIT-3 Modulo agenda

---
+++ turnos/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.admin.decorators import register
+from daterange_filter.filter import DateRangeFilter
+
+from turnos.models import DetalleTurno, Turno
+
+
+class DetalleTurnoInline(admin.TabularInline):
+    model = DetalleTurno
+    autocomplete_fields = ('servicio', )
+    extra = 0
+
+
+@register(Turno)
+class TurnoAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'cliente')
+    list_display = ('id', 'fecha', 'hora_inicio', 'hora_finalizacion', 'cliente', 'responsable')
+    list_filter = (('fecha', DateRangeFilter), )
+    autocomplete_fields = ('cliente', 'responsable')
+    inlines = (DetalleTurnoInline, )
+    actions = None
+
---
+++ turnos/models.py
@@ -0,0 +1,27 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+
+from servicios.constants import CategoriaServicio
+
+
+class Turno(models.Model):
+    fecha = models.DateField(default=datetime.date.today)
+    hora_inicio = models.TimeField(default=datetime.datetime.now)
+    hora_finalizacion = models.TimeField(blank=True, null=True)
+    cateogoria = models.CharField(max_length=3, choices=CategoriaServicio.CATEGORIAS, default=CategoriaServicio.PELUQUERIA)
+    cliente = models.ForeignKey('clientes.Cliente', on_delete=models.CASCADE)
+    responsable = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return str(self.fecha.strftime('%d/%m/%Y')) + ' - ' + str(self.hora_inicio.strftime('%H:%M')) + ' | ' + self.cliente.nombre
+
+
+class DetalleTurno(models.Model):
+    class Meta:
+        verbose_name = "Servicio"
+        verbose_name_plural = "Servicios"
+    turno = models.ForeignKey(Turno, on_delete=models.CASCADE)
+    servicio = models.ForeignKey('servicios.Servicio', on_delete=models.PROTECT)
+
