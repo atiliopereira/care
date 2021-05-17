014c1763c17f054e80cb30add6541819a208354d
chilu <atilio@dissoi.com>
2021-05-17 09:58:21 -0400

    MASQ-12 (usuario) asignar box a usuario

--- sistema/models.py
+++ sistema/models.py
@@ -6,7 +6,6 @@ from turnos.constants import BoxTurno
 
 class Usuario(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    ruc = models.CharField(max_length=20, verbose_name="RUC/CI Nro.", null=True, blank=True)
     box = models.CharField(max_length=6, choices=BoxTurno.BOXES, blank=True, null=True)
 
 
